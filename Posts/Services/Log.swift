59f053bcc82c5fd2cf477d24dbe3486bfb6a274d
Diogo Tridapalli <diogot@users.noreply.github.com>
2019-01-19 15:39:19 -0200

    Add Log
    Signed-off-by: Diogo Tridapalli <diogot@users.noreply.github.com>

---
+++ Posts/Services/Log.swift
@@ -0,0 +1,43 @@
+//
+//  Log.swift
+//  MyWeight
+//
+//  Created by Diogo Tridapalli on 9/13/16.
+//  Copyright © 2016 Diogo Tridapalli. All rights reserved.
+//
+
+import Foundation
+
+public struct Log {
+
+    public static func info(_ items: Any..., functionName: String = #function, fileName: String = #file, lineNumber: Int = #line) {
+        log(items, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+    }
+
+    public static func error(_ error: Error?, functionName: String = #function, fileName: String = #file, lineNumber: Int = #line) {
+        guard let error = error else {
+            return
+        }
+
+        debug(error, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+    }
+
+    public static func debug(_ items: Any..., functionName: String = #function, fileName: String = #file, lineNumber: Int = #line) {
+        #if DEBUG
+            log(items, functionName: functionName, fileName: fileName, lineNumber: lineNumber)
+        #endif
+    }
+
+    private static func log(_ items: [Any], functionName: String, fileName: String, lineNumber: Int) {
+        #if DEBUG
+            let url = NSURL(fileURLWithPath: fileName)
+            let lastPathComponent = url.lastPathComponent ?? fileName
+            print("[\(lastPathComponent):\(lineNumber)] \(functionName):", separator: "", terminator: " ")
+        #endif
+        for item in items {
+            print(item, separator: "", terminator: "")
+        }
+        print("")
+    }
+
+}
