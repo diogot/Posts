6204fbede4ef66f15206754f5aae91e12e506069
Diogo Tridapalli <diogot@users.noreply.github.com>
2019-01-19 16:43:20 -0200

    Add tests to NetworkRequest
    Signed-off-by: Diogo Tridapalli <diogot@users.noreply.github.com>

---
+++ PostsTests/Services/Network/NetworkRequestTests.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkRequestTests.swift
+//  PostsTests
+//
+//  Created by Diogo on 19/01/19.
+//  Copyright © 2019 Diogo. All rights reserved.
+//
+
+import Nimble
+@testable import Posts
+import XCTest
+
+final class NetworkRequestTests: XCTestCase {
+
+    private var request: NetworkRequest!
+    private var url: URL!
+    private var data: Data!
+    private var headers: [String: String]!
+
+    override func setUp() {
+        super.setUp()
+        url = URL(string: "http://diogot.com")!
+        data = "data".data(using: .utf8)!
+        headers = ["header1": "fine", "header2": "go"]
+        request = NetworkRequest(method: .get, url: url, body: data, headers: headers)
+    }
+
+    func testMethod() {
+        expect(self.request.method) == .get
+    }
+
+    func testURL() {
+        expect(self.request.url) == url
+    }
+
+    func testBody() {
+        expect(self.request.body) == data
+    }
+
+    func testHeaders() {
+        expect(self.request.headers) == headers
+    }
+
+    func testURLRequest() {
+        let urlRequest = request.urlRequest
+        expect(urlRequest.url) == url
+        expect(urlRequest.httpMethod) == "GET"
+        expect(urlRequest.httpBody) == data
+        let headers = urlRequest.allHTTPHeaderFields
+        expect(headers?["header1"]) == "fine"
+        expect(headers?["header2"]) == "go"
+    }
+}
