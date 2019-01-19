80b701f30dd37bb6f60c170bb1795a41d666a7f2
Diogo Tridapalli <diogot@users.noreply.github.com>
2019-01-19 10:38:16 -0200

    Create empty project
    Signed-off-by: Diogo Tridapalli <diogot@users.noreply.github.com>

---
+++ Posts/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Posts
+//
+//  Created by Diogo on 19/01/19.
+//  Copyright © 2019 Diogo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+
+        let navigationController = UINavigationController()
+        window.rootViewController = navigationController
+
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
